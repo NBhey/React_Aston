a32570a14f87d5702c67943ed4a1cd3980268892
Oganes <og.marckaryan@yandex.ru>
2025-03-24 12:59:53 +0300

    make list with fragment/key

--- src/MyClassComponent.js
+++ src/MyClassComponent.js
@@ -7,16 +7,20 @@ class MyClassComponent extends Component {
       inputValue: "",
       submittedValue: "",
       stateButton: false,
-      
     };
     this.inputRef = React.createRef();
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
   handleInputChange = (event) => {
-    this.setState({ inputValue: event.target.value , stateButton: event.target.value.toLowerCase().includes("react")});  
-    if(this.state.stateButton && !event.target.value.toLowerCase().includes("react")){
-      console.warn('продолжайте печатать, кнопка доступна')
+    this.setState({
+      inputValue: event.target.value,
+      stateButton: event.target.value.toLowerCase().includes("react"),
+    });
+    if (
+      this.state.stateButton &&
+      !event.target.value.toLowerCase().includes("react")
+    ) {
+      console.warn("продолжайте печатать, кнопка доступна");
     }
   };
 
@@ -29,10 +33,10 @@ class MyClassComponent extends Component {
   }
 
   focusInput = (e) => {
-    e.preventDefault()
-    console.log('отработал')
-    this.inputRef.current.focus()
-  }
+    e.preventDefault();
+    console.log("отработал");
+    this.inputRef.current.focus();
+  };
 
   componentDidMount() {
     console.log("Компонент смонтирован");
@@ -59,13 +63,18 @@ class MyClassComponent extends Component {
               onChange={this.handleInputChange}
             />
           </label>
-          <button type="submit" disabled = {this.state.stateButton}>Отправить</button>
-          <button type="click" onClick={this.focusInput}>Фокус на input</button>
+          <button type="submit" disabled={this.state.stateButton}>
+            Отправить
+          </button>
+          <button type="click" onClick={this.focusInput}>
+            Фокус на input
+          </button>
         </form>
         <ChildComponent
           value={this.state.submittedValue}
           cb={() => console.log("Ребенок смонтирован!")}
         />
+        <ListWithKey dataList={list} />
       </div>
     );
   }
@@ -95,5 +104,22 @@ class ChildComponent extends Component {
     );
   }
 }
+
+const list = [{ name: "John" }, { name: "Pak" }, { name: "Ivan" }];
+const ListWithKey = ({ dataList }) => {
+  return (
+    <ul>
+      {dataList.map((el, i) => {
+        return (
+          <React.Fragment
+            key={`${i}_${Math.random().toString(36).substr(2, 9)}`}
+          >
+            <li>{el.name}</li>
+          </React.Fragment>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default MyClassComponent;
